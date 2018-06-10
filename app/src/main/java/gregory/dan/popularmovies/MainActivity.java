812ee8759446d919d6a88ad472cbbb1bd058b6a5
Dan Gregory <danielgregory1986@googlemail.com>
2018-06-10 11:00:35 +0100

    Input stream coming up as null

--- app/src/main/java/gregory/dan/popularmovies/MainActivity.java
+++ app/src/main/java/gregory/dan/popularmovies/MainActivity.java
@@ -1,11 +1,17 @@
 package gregory.dan.popularmovies;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import java.io.IOException;
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity implements MyRecyclerViewAdapter.ListItemClickListener{
 
@@ -14,6 +20,7 @@ public class MainActivity extends AppCompatActivity implements MyRecyclerViewAda
 
     RecyclerView recyclerView;
     MyRecyclerViewAdapter adapter;
+    TextView testText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,7 +33,12 @@ public class MainActivity extends AppCompatActivity implements MyRecyclerViewAda
         int numCols = 2;
         recyclerView.setLayoutManager(new GridLayoutManager(this, numCols));
         adapter = new MyRecyclerViewAdapter(this, data, this);
-        recyclerView.setAdapter(adapter);
+//        recyclerView.setAdapter(adapter);
+
+        //test text view to be deleted
+        testText = (TextView) findViewById(R.id.test_text_view);
+
+        new MyNetworkTasker().execute("test");
 
     }
 
@@ -41,5 +53,36 @@ public class MainActivity extends AppCompatActivity implements MyRecyclerViewAda
         Toast mToast = Toast.makeText(this, toastMessage, Toast.LENGTH_LONG);
 
         mToast.show();
+    }
+
+    public void mSetText(String result){
+        testText.setText(result);
+    }
+
+    public class MyNetworkTasker extends AsyncTask<String, Void, String> {
+
+        @Override
+        protected String doInBackground(String... params) {
+            if(params.length == 0){
+                return null;
+            }
+
+            int listType = 0;
+            URL searchURL = MyMovieFetcher.buildUrl(listType);
+            String results = null;
+            try{
+                results = MyMovieFetcher.getResponseFromHttpUrl(searchURL);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return results;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            Log.d("test", "onPostExecute: finished");
+//            mSetText(s);
+
+        }
     }
 }
